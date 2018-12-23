f181e28f7203b873378cab7cc03e466463fefa72
Marta Puszyńska <marta.puszynska93@gmail.com>
2018-12-23 19:05:20 +0100

    animations-javascript-code

--- 2-midi-controller/js/script.js
+++ 2-midi-controller/js/script.js
@@ -1,23 +1,27 @@
 "use strict";
 
-
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const blackKey = document.querySelector(`.black-key[data-key="${e.keyCode}"]`);
     const pad = document.querySelector(`.pad[data-key="${e.keyCode}"]`);
 
-    if(!audio) return;
+    if(!audio) {
+        return;
+    } else if (!key && !blackKey) {
+        pad.classList.add("pad-tap");
+    } else if(!pad && !blackKey) {
+        key.classList.add("key-click");
+    } else if(!key && !pad) {
+        blackKey.classList.add("black-key-click");
+    };
+
     audio.currentTime = 0;
     audio.play();
-    key.classList.add("key-click");
-    blackKey.classList.add("black-key-click");
-    pad.classList.add("pad-tap");
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== "transform") return;
-    this.classList.remove("key-click", "black-key-click", "pad-tap"); // removing the class
+    this.classList.remove("key-click", "black-key-click", "pad-tap");
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
@@ -32,5 +36,3 @@ pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
 
 window.addEventListener("keydown", playSound);
 
-
-//  counter 4s !!!
