7e8f11fa4b6c2d28790dd977727c36fa1d19281a
Marta Puszyńska <marta.puszynska93@gmail.com>
2019-01-15 18:37:22 +0100

    rwd-added-js-modification

--- 4-photography/js/script.js
+++ 4-photography/js/script.js
@@ -9,6 +9,8 @@ function toggleSidebar() {
 button.onclick = toggleSidebar;
 
 // OPACITY CHANGE ON SCROLL
+var photo1 = $('#photo-container1');
+var photo2 = $('#photo-container2');
 var photo3 = $('#photo-container3');
 var photo4 = $('#photo-container4');
 var photo5 = $('#photo-container5');
@@ -24,27 +26,59 @@ doc.ready(function () {
     // console.log(win.scrollTop());
     win.scroll(function () {
         console.log(win.scrollTop());
-        if (($(this).scrollTop() > 200) && ($(this).scrollTop() < 900)) {
-            photo3.css({ "opacity": "1" })
-            photo4.css({ "opacity": "1" })
-            photo5.css({ "opacity": "1" })
-        } else if (($(this).scrollTop() >= 900) && ($(this).scrollTop() < 1300)) {
-            photo6.css({ "opacity": "1" })
-        } else if (($(this).scrollTop() >= 1300) && ($(this).scrollTop() < 1700)) {
-            photo7.css({ "opacity": "1" })
-            photo8.css({ "opacity": "1" })
-        } else if ($(this).scrollTop() > 1700) {
-            photo9.css({ "opacity": "1" })
-            photo10.css({ "opacity": "1" })
+        if (win.width > 768) {
+            if (($(this).scrollTop() > 200) && ($(this).scrollTop() < 900)) {
+                photo3.css({ "opacity": "1" })
+                photo4.css({ "opacity": "1" })
+                photo5.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 900) && ($(this).scrollTop() < 1300)) {
+                photo6.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 1300) && ($(this).scrollTop() < 1700)) {
+                photo7.css({ "opacity": "1" })
+                photo8.css({ "opacity": "1" })
+            } else if ($(this).scrollTop() > 1700) {
+                photo9.css({ "opacity": "1" })
+                photo10.css({ "opacity": "1" })
+            } else {
+                photo3.css({ "opacity": "0.4" })
+                photo4.css({ "opacity": "0.4" })
+                photo5.css({ "opacity": "0.4" })
+                photo6.css({ "opacity": "0.4" })
+                photo7.css({ "opacity": "0.4" })
+                photo8.css({ "opacity": "0.4" })
+                photo9.css({ "opacity": "0.4" })
+                photo10.css({ "opacity": "0.4" })
+            };
         } else {
-            photo3.css({ "opacity": "0.4" })
-            photo4.css({ "opacity": "0.4" })
-            photo5.css({ "opacity": "0.4" })
-            photo6.css({ "opacity": "0.4" })
-            photo7.css({ "opacity": "0.4" })
-            photo8.css({ "opacity": "0.4" })
-            photo9.css({ "opacity": "0.4" })
-            photo10.css({ "opacity": "0.4" })
-        };
+            if (($(this).scrollTop() > 200) && ($(this).scrollTop() < 500)) {
+                photo2.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 500) && ($(this).scrollTop() < 800)) {
+                photo3.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 800) && ($(this).scrollTop() < 1200)) {
+                photo4.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 1200) && ($(this).scrollTop() < 2200)) {
+                photo5.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 2200) && ($(this).scrollTop() < 2700)) {
+                photo6.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 2700) && ($(this).scrollTop() < 3000)) {
+                photo7.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 3000) && ($(this).scrollTop() < 3400)) {
+                photo8.css({ "opacity": "1" })
+            } else if (($(this).scrollTop() >= 3400) && ($(this).scrollTop() < 3600)) {
+                photo9.css({ "opacity": "1" })
+            } else if ($(this).scrollTop() >= 3600) {
+                photo10.css({ "opacity": "1" })
+            } else {
+                photo2.css({ "opacity": "0.4" })
+                photo3.css({ "opacity": "0.4" })
+                photo4.css({ "opacity": "0.4" })
+                photo5.css({ "opacity": "0.4" })
+                photo6.css({ "opacity": "0.4" })
+                photo7.css({ "opacity": "0.4" })
+                photo8.css({ "opacity": "0.4" })
+                photo9.css({ "opacity": "0.4" })
+                photo10.css({ "opacity": "0.4" })
+            };
+        }
     })
 })
