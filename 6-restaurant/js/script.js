cc7f993e0ec6f2ee155fa038c2548e463fba3b93
Marta Puszyńska <marta.puszynska93@gmail.com>
2019-02-07 20:16:09 +0100

    restaurant-code-cleaning-smooth-scroll

--- 6-restaurant/js/script.js
+++ 6-restaurant/js/script.js
@@ -1,22 +1,26 @@
 'use strict';
 
-var win = $(window);
-var doc = $(document);
-var nav = $('#nav-bar');
+var nav = $('.nav-bar');
 
-doc.ready(function () {
-    win.scroll(function () {
+$(document).ready(function () {
+    $(window).scroll(function () {
         // console.log(win.scrollTop());
-        if (win.scrollTop()) {
+        if ($(window).scrollTop()) {
             // nav.css({ 'background-color': 'rgb(38,38,38)' })
-            nav.addClass("bar-scroll");
+            nav.addClass("nav-bar-scroll");
         } else {
-            nav.removeClass("bar-scroll");
+            nav.removeClass("nav-bar-scroll");
         }
-    })
+    });
+
+    nav.localScroll();
 })
 
 
+
+
+
+// NIE DZIAŁA
 var links = $(".link");
 
 // links.each(function () {
